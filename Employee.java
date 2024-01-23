1e575883f72334d83828613259884f75af31b4f6
Rishbh Sinha <sinharishabh@gmail.com>
2024-01-23 11:46:44 +0530

    ATTENDENCE CHECK

--- Employee.java
+++ Employee.java
@@ -3,12 +3,17 @@ public class Employee
     String name;
     Integer id;
     boolean attendence;
+    Integer wagePerHour;
+    Integer workHour=0;
 
 
-    Employee(String n,Integer i)
+
+
+    Employee(String n,Integer i,Integer w)
     {
         this.name=n;
         this.id=i;
+        this.wagePerHour=w;
     }
 
 
--- Main.java
+++ Main.java
@@ -1,26 +1,30 @@
-public class Main
-{
-    public static void main(String[] args)
-{
-    Employee e1 = new Employee("Rishabh",1);
-    int check = (int)((Math.random()*10))%2;
+public class Main {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Bharat", 1, 200);
+        int check = (int) ((Math.random() * 10)) % 2;
 
-    if(check==0)
-    {
-        (e1.attendence)=false;
+        if (check == 0) {
+            e1.attendence = false;
+        } else {
+            e1.attendence = true;
+        }
+
+        if (e1.attendence) {
+            System.out.println("Employee is present");
+            e1.workHour = 8;
+        } else {
+            System.out.println("Employee is absent");
+        }
+
+        //it is a Not static method
+        Main m1 = new Main();
+        System.out.println(m1.calculateSalary(e1));
     }
-    else {
-        (e1.attendence) = true;
-    }
-    if(e1.attendence) {
-        System.out.println("he is present");
-    }
-    else{
-        System.out.println("he is absent");
 
 
-    }
+        double calculateSalary (Employee e)
+        {
+            return e.workHour * e.wagePerHour;
+        }
 
 }
-
-    }
